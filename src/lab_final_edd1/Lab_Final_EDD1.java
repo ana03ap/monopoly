d09315868fafcb3140a9d577640cb54ce98a6642
Ana Ardila <90521002+ana03ap@users.noreply.github.com>
2022-05-09 19:55:40 -0500

    add proyecto
    .

---
+++ src/lab_final_edd1/Lab_Final_EDD1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_final_edd1;
+
+/**
+ *
+ * @author Hp
+ */
+public class Lab_Final_EDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pp g = new Pp();
+        g.setVisible(true);
+    }
+    
+}
