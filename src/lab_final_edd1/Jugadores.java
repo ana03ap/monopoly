dd83665c36fdf0e06496afdafe9fc52a7e0a9c5d
EmilyRoldan <eroldan@uninorte.edu.co>
2022-05-10 11:51:27 -0500

    update

--- src/lab_final_edd1/Jugadores.java
+++ src/lab_final_edd1/Jugadores.java
@@ -10,5 +10,10 @@ package lab_final_edd1;
  * @author Emily Roldán
  */
 public class Jugadores {
-    
+    String color;
+    int posicion;
+    int estado; // 0 jugando, 1 bancarrota, 2 encarcelado
+    String propiedades; // esto es una lista de propiedades 
+    String monopolios; // esto es una lista dependiendo de propiedades BONOOOOO
+    int dinero;
 }
